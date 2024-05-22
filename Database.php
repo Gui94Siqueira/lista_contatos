7645e953bafe42ee6f10403e68992012ad4a8095
Guilherme <guilherme94siqueira@gmail.com>
2024-05-22 20:42:16 -0300

    Telas de index e datalhes com mock

--- Database.php
+++ Database.php
@@ -4,14 +4,14 @@
         private $conn;
         private $host = 'localhost';
         private $db = 'contatos';
-        private $user= 'root';
-        private $pass = "";
+        private $user = 'root';
+        private $pass = '';
 
-        private function __construct()
+        public function __construct()
         {
             try 
             {
-                $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db, $this->user, $this->pass");
+                $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e)
             {
